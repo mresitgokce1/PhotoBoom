cd6326985295abbfcf4e63801c73260f4ab45e20
mresitgokce1 <gQaZ77o16o9b>
2021-06-13 19:02:58 +0300

    Added business layer

---
+++ PhotoBoom.Business/Abstract/IPhotoService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhotoBoom.Entities;
+
+namespace PhotoBoom.Business.Abstract
+{
+    public interface IPhotoService
+    {
+        List<Photo> GetAllPhotos();
+        Photo GetHotelById(int id);
+        Photo CreatePhoto(Photo photo);
+        Photo UpdatePhoto(Photo photo);
+        void DeletePhoto(int id);
+    }
+}
---
+++ PhotoBoom.Business/Concrete/PhotoManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhotoBoom.Business.Abstract;
+using PhotoBoom.Entities;
+using PhotoBoom.DataAccess.Abstract;
+using PhotoBoom.DataAccess.Concrete;
+
+namespace PhotoBoom.Business.Concrete
+{
+    public class PhotoManager : IPhotoService
+    {
+        private IPhotoRepository _photoRepository;
+
+        public PhotoManager()
+        {
+            _photoRepository = new PhotoRepository();
+        }
+        public Photo CreatePhoto(Photo photo)
+        {
+            return _photoRepository.CreatePhoto(photo);
+        }
+
+        public void DeletePhoto(int id)
+        {
+            _photoRepository.DeletePhoto(id);
+        }
+
+        public List<Photo> GetAllPhotos()
+        {
+            return _photoRepository.GetAllPhotos();
+        }
+
+        public Photo GetHotelById(int id)
+        {
+            return _photoRepository.GetPhotoById(id);
+        }
+
+        public Photo UpdatePhoto(Photo photo)
+        {
+            return _photoRepository.UpdatePhoto(photo);
+        }
+    }
+}
